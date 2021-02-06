863622f024a1eee7f260206c757de2f9dafbf23d
sk192 <korulkar.shweta7@gmail.com>
2021-02-06 01:03:40 -0800

    react weather app. uptil now created weather page with navbar and search box. Page linking and fetching data from API is still remaining.

---
+++ weather-app/src/App.js
@@ -0,0 +1,15 @@
+
+// import Button from 'react-bootstrap/Button';
+import React, { Component } from 'react'; 
+// import { Navbar, Nav, NavItem, Button,NavDropdown, FormControl,MenuItem,Form,Container } from 'react-bootstrap';
+import './App.css';
+import Navbar from './components/header/Navbar';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+      <Navbar />
+  );
+}
+
+export default App;
