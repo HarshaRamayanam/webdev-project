1a203ac8a28ae2cd25edcc486fd4347175e54cf5
HarshaRamayanam <63016598+HarshaRamayanam@users.noreply.github.com>
2021-02-07 18:52:38 -0700

    Replicated Navbar component from main app

--- app/src/App.js
+++ app/src/App.js
@@ -1,17 +1,15 @@
 // import logo from './logo.svg';
 
 // importing stylings
-import './App.css';
+import "./App.css";
+import MyNavbar from "./components/Navbar";
 
 // necessary imports
-import React, {useState, useEffect} from 'react';
-
+import React, { useState, useEffect } from "react";
 
 // main App component
 function App() {
-  return(
-    <h1>Hello React</h1>
-  )
+  return <MyNavbar />;
 }
 
 export default App;
---
+++ app/src/components/Navbar.js
@@ -0,0 +1,42 @@
+import { Navbar, Nav } from "react-bootstrap";
+import logo from "../images/weather_icon.png";
+
+const MyNavBar = () => {
+  return (
+    <div>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar.Brand href="#home">
+          <img
+            src={logo}
+            width="40"
+            height="40"
+            className="d-inline-block align-top"
+            alt="Weather logo"
+          />{" "}
+          Weather
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#Home">Home</Nav.Link>
+            <Nav.Link href="#Hike_Trails">Hike_Trails</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <div>
+        <div id="flex-container">
+          <div>
+            <h3>Get weather forecast </h3>
+            <input
+              type="text"
+              placeholder="Enter city name"
+              className="searchBar"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MyNavBar;
