b609c9673ad2e5ae1e6be7daefc9d3a83d01a394
HarshaRamayanam <63016598+HarshaRamayanam@users.noreply.github.com>
2021-02-07 18:17:49 -0700

    Created a boiler template in "src/App.js"

--- app/src/App.js
+++ app/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+
+// importing stylings
 import './App.css';
 
+// necessary imports
+import React, {useState, useEffect} from 'react';
+
+
+// main App component
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return(
+    <h1>Hello React</h1>
+  )
 }
 
 export default App;
