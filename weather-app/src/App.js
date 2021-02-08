fbdc418e6adb69dfc8b95e249bb3b3f0bd8ba0a6
HarshaRamayanam <63016598+HarshaRamayanam@users.noreply.github.com>
2021-02-07 19:52:09 -0700

    Modified "App.js" accordingly to the new directory structure.

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,14 +1,22 @@
-
 // import Button from 'react-bootstrap/Button';
-import React, { Component } from 'react'; 
+import React, { Component } from "react";
 // import { Navbar, Nav, NavItem, Button,NavDropdown, FormControl,MenuItem,Form,Container } from 'react-bootstrap';
-import './App.css';
-import Navbar from './components/header/Navbar';
+import "./App.css";
+
+// import Navbar from "./components/header/Navbar";
+// Use the following import instead of above
+import WeatherNavbar from "./components/navbars/WeatherNavbar.js";
+
 import "bootstrap/dist/css/bootstrap.min.css";
+
+// Code by Harsha. Just to differentiate
+import Link from "react-router-dom";
 
 function App() {
   return (
-      <Navbar />
+    <div className="nav-container">
+      <WeatherNavbar />
+    </div>
   );
 }
 
