e376293a27d7b68e8c6ef1e9248bb7fa6af28fba
HarshaRamayanam <63016598+HarshaRamayanam@users.noreply.github.com>
2021-02-16 14:57:09 -0700

    Renamed "Nav.js" -> "Header.js"

---
+++ weather-app/src/components/Header.js
@@ -0,0 +1,52 @@
+import { Navbar, Nav } from "react-bootstrap";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+function Header() {
+  const navStyle = {
+    color: "white",
+  };
+
+  return (
+    <Router>
+      <div className="navbar">
+        <nav>
+          <br />
+          <h3>Logo</h3>
+          <ul className="nav-links">
+            <Link
+              style={navStyle}
+              to="/"
+              onClick={() => {
+                window.location.href = "/";
+              }}
+            >
+              <li>Home</li>
+            </Link>
+
+            <Link
+              style={navStyle}
+              to="/weather"
+              onClick={() => {
+                window.location.href = "/weather";
+              }}
+            >
+              <li>Weather</li>
+            </Link>
+
+            <Link
+              style={navStyle}
+              to="/hike-trails"
+              onClick={() => {
+                window.location.href = "/hike-trails";
+              }}
+            >
+              <li>HikeTrails</li>
+            </Link>
+          </ul>
+        </nav>
+      </div>
+    </Router>
+  );
+}
+
+export default Header;
