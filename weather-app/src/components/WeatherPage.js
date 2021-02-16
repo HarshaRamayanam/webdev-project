d80e89c36e4184ef11e458b8179b7489b3782440
sk192 <korulkar.shweta7@gmail.com>
2021-02-15 16:23:42 -0800

    imported css file

--- weather-app/src/components/WeatherPage.js
+++ weather-app/src/components/WeatherPage.js
@@ -1,13 +1,13 @@
 // importing required modules
 import React, { Component } from 'react'; 
-import './style.css';
-
+import '../App.css';
 import Weather_form from './form.js'
 
 function Weather() {
   return (
     <div className="weather_page">
         <Weather_form />
+        
       
     </div>
      
