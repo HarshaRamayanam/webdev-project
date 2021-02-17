c6adf8958ee88df74bda28d0c64ac643b790954a
HarshaRamayanam <63016598+HarshaRamayanam@users.noreply.github.com>
2021-02-17 14:32:58 -0700

    Cleaned up a bit. Used "ErrorPage.js" for routing invalid urls

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,38 +1,26 @@
-
-// import Button from 'react-bootstrap/Button';
 import React from "react";
 import "./App.css";
 // importing components
 import HomePage from "./components/HomePage";
 import HikeTrailsPage from "./components/HikeTrailsPage";
 import WeatherPage from "./components/WeatherPage";
-import Nav from './components/Nav.js';
+import Header from "./components/Header.js";
+import ErrorPage from "./components/ErrorPage";
 
 // importing react modules required to redirect pages
 import { Switch, Route, Link, BrowserRouter } from "react-router-dom";
 
-
 function App() {
   return (
     <BrowserRouter>
-      <div>
-        <Nav />
-        <div>
-          <Switch>
-            <Route exact path="/" component={HomePage} />
-            <Route exact path="/hike-trails" component={HikeTrailsPage} />
-            <Route exact path="/weather" component={WeatherPage} />
-            <Route
-              render={() => {
-                return <p>Page not found</p>;
-              }}
-            />
-          </Switch>
-        </div>
-
-      </div>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/hike-trails" component={HikeTrailsPage} />
+        <Route exact path="/weather" component={WeatherPage} />
+        <Route component={ErrorPage} />
+      </Switch>
     </BrowserRouter>
-
   );
 }
 export default App;
