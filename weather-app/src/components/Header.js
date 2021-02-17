ad49077521621726c6841ab9111f44452bc473e0
HarshaRamayanam <63016598+HarshaRamayanam@users.noreply.github.com>
2021-02-17 14:33:31 -0700

    Created new component "ErrorPage.js" for any errors in URL

--- weather-app/src/components/Header.js
+++ weather-app/src/components/Header.js
@@ -1,5 +1,4 @@
-import { Navbar, Nav } from "react-bootstrap";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Link } from "react-router-dom";
 
 function Header() {
   const navStyle = {
@@ -7,47 +6,45 @@ function Header() {
   };
 
   return (
-    <header>
-      <Router>
-        <div className="navbar">
-          <nav>
-            <br />
-            <h3>Logo</h3>
-            <ul className="nav-links">
-              <Link
-                style={navStyle}
-                to="/"
-                onClick={() => {
-                  window.location.href = "/";
-                }}
-              >
-                <li>Home</li>
-              </Link>
+    <Router>
+      <div className="navbar">
+        <nav>
+          <br />
+          <h3>Logo</h3>
+          <ul className="nav-links">
+            <Link
+              style={navStyle}
+              to="/"
+              onClick={() => {
+                window.location.href = "/";
+              }}
+            >
+              <li>Home</li>
+            </Link>
 
-              <Link
-                style={navStyle}
-                to="/weather"
-                onClick={() => {
-                  window.location.href = "/weather";
-                }}
-              >
-                <li>Weather</li>
-              </Link>
+            <Link
+              style={navStyle}
+              to="/weather"
+              onClick={() => {
+                window.location.href = "/weather";
+              }}
+            >
+              <li>Weather</li>
+            </Link>
 
-              <Link
-                style={navStyle}
-                to="/hike-trails"
-                onClick={() => {
-                  window.location.href = "/hike-trails";
-                }}
-              >
-                <li>HikeTrails</li>
-              </Link>
-            </ul>
-          </nav>
-        </div>
-      </Router>
-    </header>
+            <Link
+              style={navStyle}
+              to="/hike-trails"
+              onClick={() => {
+                window.location.href = "/hike-trails";
+              }}
+            >
+              <li>HikeTrails</li>
+            </Link>
+          </ul>
+        </nav>
+      </div>
+    </Router>
   );
 }
 
