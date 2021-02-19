68b4fcfc365b4e8b3110073826fc4faeaafefa11
HarshaRamayanam <63016598+HarshaRamayanam@users.noreply.github.com>
2021-02-18 17:26:29 -0700

    Refactored Routing code

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,26 +1,29 @@
 import React from "react";
 import "./App.css";
+
 // importing components
 import HomePage from "./components/HomePage";
 import HikeTrailsPage from "./components/HikeTrailsPage";
 import WeatherPage from "./components/WeatherPage";
-import Header from "./components/Header.js";
+import HeaderNav from "./components/HeaderNav.js";
+import Footer from "./components/Footer";
 import ErrorPage from "./components/ErrorPage";
 
 // importing react modules required to redirect pages
-import { Switch, Route, Link, BrowserRouter } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Header />
+    <Router>
+      <HeaderNav />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/hike-trails" component={HikeTrailsPage} />
         <Route exact path="/weather" component={WeatherPage} />
         <Route component={ErrorPage} />
       </Switch>
-    </BrowserRouter>
+      <Footer />
+    </Router>
   );
 }
 export default App;
