58789cf9fba690a4565c7cbdf74223fef16176f9
sk192 <korulkar.shweta7@gmail.com>
2021-02-20 16:29:07 -0800

    form to seach place to get weather information

--- weather-app/src/components/form.js
+++ weather-app/src/components/form.js
@@ -1,20 +1,35 @@
-import React from 'react'
+import React from "react";
 
-function Weather_form () {
+const WeatherForm = (props) => {
+  return (
+    <div className="flex-container">
+      <div>{props.error ? error() : null}</div>
+      <form onSubmit={props.loadweather}>
+        <div className="row">
+          <div className="col-md-7 offset-md-2">
+            <input
+              type="text"
+              placeholder="Enter city name"
+              className="form-control"
+              name="city"
+              autoComplete="off"
+            />
+          </div>
+          <div>
+            <button className="btn btn-primary">Search</button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+};
 
-    return(
+function error() {
+  return (
+    <div classNames="alert alert-danger mx-5" role="alert">
+      Please Enter City
+    </div>
+  );
+}
 
-                <div>
-                    <div className="flex-container">
-                        <div>
-                        <h4>Get weather forecast </h4>
-                        <input type="text" placeholder="Enter city name"  className="searchBar" />
-                        <button className="button" type="button">Search</button>
-                        </div>
-                    </div>
-                </div>
-            
-    );
-        }
-
-export default Weather_form
+export default WeatherForm;
