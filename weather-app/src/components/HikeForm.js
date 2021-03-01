9c58d9706876960b54dd46377839a5354df529dd
HarshaRamayanam <63016598+HarshaRamayanam@users.noreply.github.com>
2021-02-28 18:35:41 -0700

    Worked on HikeForm.js to create a structure

--- weather-app/src/components/HikeForm.js
+++ weather-app/src/components/HikeForm.js
@@ -1,18 +1,71 @@
-import { Form, FormControl, Button } from "react-bootstrap";
+import React, { useState, useEffect } from "react";
+import HikeDisplay from "./HikeDisplay";
+import "./stylesheets/HikeForm.css";
 
 function HikeForm() {
+  const [searchBy, setSearchBy] = useState("hikeName");
+  const [trailData, setTrailData] = useState([]);
+  const [userInput, setUserInput] = useState("");
+  const [formSubmit, setFormSubmit] = useState(false);
+
+  // console.log(searchBy);
+
+  async function fetchTrailData() {
+    const PATH_TO_JSON_DATA = `./datasets/hiketrails.json`;
+    const response = await fetch(PATH_TO_JSON_DATA);
+    const data = await response.json();
+    setTrailData(data);
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setUserInput(document.querySelector("#searchInput").value);
+    setSearchBy(document.querySelector("#mySelect").value);
+    setFormSubmit(true);
+  };
+
+  useEffect(() => {
+    fetchTrailData().catch((err) => console.log("Error occurred!\n", err));
+  }, []);
+
   return (
-    <Form className="flex-container">
-      <h4>Let's go hiking!</h4>
-      <FormControl
-        type="text"
-        placeholder="Search for hike trails"
-        className="mr-sm-2 searchBar"
-      />
-      <Button variant="light" className="button">
-        Search
-      </Button>
-    </Form>
+    <div className="container mx-auto">
+      <form onSubmit={handleSubmit}>
+        <div className="form-group input-group">
+          <div className="input-group-prepend">
+            <select className="form-control form-select btn-dark" id="mySelect">
+              <option value="hikeName" defaultValue>
+                Search By Hike Name
+              </option>
+              <option value="loc">Search By Hike Location</option>
+            </select>
+          </div>
+
+          <label htmlFor="searchInput"></label>
+          <input
+            type="text"
+            id="searchInput"
+            className="form-control"
+            placeholder="Search for hikes"
+            required
+          />
+          <div className="input-group-append">
+            <button type="submit" className="btn btn-primary">
+              <i className="fa fa-search"></i>
+            </button>
+          </div>
+        </div>
+      </form>
+      <div>
+        {formSubmit ? (
+          <HikeDisplay
+            data={trailData}
+            searchBy={searchBy}
+            userInput={userInput}
+          />
+        ) : null}
+      </div>
+    </div>
   );
 }
 
