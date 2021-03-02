ac7b4f0cf20b93e32836449c7c5abc7063c918a9
sk192 <korulkar.shweta7@gmail.com>
2021-03-01 16:10:16 -0800

    chnaged class name

---
+++ weather-app/src/components/Weather_page/Weather_card.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "./WeatherCard.css";
+
+const WeatherCard = (props) => {
+  return (
+    <div className="row">
+        <div className="cards">
+          <div className="card_info">
+            <div className="d-flex justify-content-between">
+              <div className="city_name">
+                  <h4><b>{props.city}</b></h4>
+                  <h6>{props.dateTime}</h6>  
+                  <h6>{props.Day}</h6>                  
+              </div>
+              <div className="we_icon ">
+                <img  src={`http://openweathermap.org/img/w/${props.weather_icon}.png`} class="img-fluid" alt="weather icon" width="80px" />
+                <h4 className="px-2">{props.description}</h4>
+              </div>
+              <div className="city_temp">
+                  {props.temperature ? (
+                    <h1 className="px-5"><b>{props.temperature}&deg;c</b></h1>
+                  ) : null}
+                
+                  {props.feelsLike ? (
+                  <h4 className="px-5 py-1" >Feels Like: <b>{props.feelsLike}&deg;</b></h4>
+                  ) : null}
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+  );
+};
+
+export default WeatherCard;
