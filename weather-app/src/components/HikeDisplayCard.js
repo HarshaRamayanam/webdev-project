ddddb18e390c92c5844ec0627d1c6a4840fe908c
HarshaRamayanam <63016598+HarshaRamayanam@users.noreply.github.com>
2021-03-01 18:38:09 -0700

    Worked on star rating CSS for hike display

--- weather-app/src/components/HikeDisplayCard.js
+++ weather-app/src/components/HikeDisplayCard.js
@@ -2,11 +2,28 @@ import React from "react";
 import "./stylesheets/HikeDisplayCard.css";
 
 const HikeDisplayCard = (props) => {
+  const starTotal = 5;
+  let starPercentage = (props.avg_rating / starTotal) * 100;
+  let starPercentageRounded = `${Math.round(starPercentage / 10) * 10}%`;
+  let stylingObject = {
+    "span.stars-inner": {
+      width: starPercentageRounded,
+    },
+  };
+
   return (
     <div className="card-container">
       <div className="card-info">
         <h2 className="card-title">{props.name}</h2>
-        <span className="stats">Rating: &nbsp; {props.avg_rating} / 5</span>
+        <span className="statss">
+          <span className="stars-outer">
+            <span
+              className="stars-inner"
+              style={stylingObject["span.stars-inner"]}
+            ></span>
+          </span>
+          &nbsp;{props.avg_rating}
+        </span>
         <span className="stats">
           Difficulty: &nbsp; {props.difficulty_rating}
         </span>
