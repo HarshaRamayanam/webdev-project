2cc2d159c26c99ddf29b7cd369ac4c928328b57b
sk192 <korulkar.shweta7@gmail.com>
2021-03-05 13:52:23 -0800

    modified function to async function

--- weather-app/src/components/chart/lineChart.js
+++ weather-app/src/components/chart/lineChart.js
@@ -25,48 +25,48 @@ class LineChart extends Component{
             this.componentDidMount();
         }
     }
-    componentDidMount(){
+    componentDidMount = async() =>{
         try{
+          if(this.props.lat !== undefined && this.props.lon !== undefined){
 
-          axios.get("https://api.openweathermap.org/data/2.5/onecall?lat="+this.props.lat+"&lon="+this.props.lon+"&exclude=minutely&appid="+ApiKey+"")
-            .then((response) =>{
-              if (response.status === 400 || response.status === 500 || response.status === 404) {
-                console.log("please enter valid city");
+            const response = await axios.get("https://api.openweathermap.org/data/2.5/onecall?lat="+this.props.lat+"&lon="+this.props.lon+"&exclude=minutely&appid="+ApiKey+"")
+                if (response.status === 400 || response.status === 500 || response.status === 404) {
+                  console.log("please enter valid city");
+                }
+
+              const hourly_temp = [];
+              const date_time = [];
+              for(let i=0 ;i <= 24; i++){
+                  hourly_temp.push(this.calCelcius(response.data["hourly"][i].temp));
+                  date_time.push(moment.unix(response.data["hourly"][i].dt).format("hh:mm a"));
               }
-
-            const hourly_temp = [];
-            const date_time = [];
-            for(let i=0 ;i <= 24; i++){
-                hourly_temp.push(this.calCelcius(response.data["hourly"][i].temp));
-                date_time.push(moment.unix(response.data["hourly"][i].dt).format("hh:mm a"));
+          
+              this.setState({
+                  daily_data: {
+                    labels: date_time,
+                    datasets: [
+                      {
+                        label: "Hourly",
+                        data: hourly_temp,
+                        fill: true,
+                        lineTension: 0.2,
+                        backgroundColor: " #9999ff",
+                        pointBorderColor: "#000000",
+                        pointBackgroundColor: "#000000",
+                        pointBorderWidth: 1,
+                        pointHoverRadius: 4,
+                        pointHoverBackgroundColor: "#000000",
+                        pointHoverBorderColor: "#000000",
+                        pointHoverBorderWidth: 2,
+                        pointRadius: 2,
+                        pointHitRadius: 11,
+                      },
+                    ],
+                  },
+                });
+              }; 
             }
         
-            this.setState({
-                daily_data: {
-                  labels: date_time,
-                  datasets: [
-                    {
-                      label: "Hourly",
-                      data: hourly_temp,
-                      fill: true,
-                      lineTension: 0.2,
-                      backgroundColor: "#41d33b",
-                      pointBorderColor: "#00a2ff",
-                      pointBackgroundColor: "#00a2ff",
-                      pointBorderWidth: 1,
-                      pointHoverRadius: 4,
-                      pointHoverBackgroundColor: "#00a2ff",
-                      pointHoverBorderColor: "#00a2ff",
-                      pointHoverBorderWidth: 2,
-                      pointRadius: 2,
-                      pointHitRadius: 11,
-                    },
-                  ],
-                },
-              });
-            }); 
-        
-        }
         catch(err){
 
             console.log(err);
