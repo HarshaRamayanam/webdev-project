3c5df4cf687862e1bbe6f8beb9d8b383606fc54a
HarshaRamayanam <63016598+HarshaRamayanam@users.noreply.github.com>
2021-03-07 18:01:31 -0700

    New code added for getting weather data based on lat, lng values in context variable.

--- weather-app/src/components/WeatherPage.js
+++ weather-app/src/components/WeatherPage.js
@@ -1,5 +1,5 @@
 // importing required modules
-import React, { Component,useContext } from "react";
+import React, { Component, useContext } from "react";
 import "../App.css";
 import WeatherForm from "./weather_form/WeatherForm.js";
 import WeatherCard from "./Weather_page/Weather_card";
@@ -31,15 +31,16 @@ import fifty_day from "./background_images/50d.jpg";
 import fifty_night from "./background_images/50n.jpg";
 import mist from "./background_images/mist.jpg";
 import "../App.css";
-import { BgContext } from "./ContextStore";
-
+import { LocationContext } from "./ContextStore"; // Imported this to use that global context variable
 
 require("dotenv").config();
 
 const ApiKey = process.env.REACT_APP_API_KEY;
 
 class Weather extends Component {
-  // const [location, setLocation] = useContext(LocationContext);
+  // Harsha's code - START
+  static contextType = LocationContext; // This is the way of using hooks in class components
+  // Harsha's code - END
   constructor() {
     super();
     this.state = {
@@ -55,119 +56,152 @@ class Weather extends Component {
       daily_info: [],
       day: undefined,
       dir: "",
-      
     };
   }
- setBackground = (ICON) => {
-   switch(ICON){
+  setBackground = (ICON) => {
+    switch (ICON) {
       case "01d":
-          this.setState({
-              dir: one_day
-            });
-          break;
+        this.setState({
+          dir: one_day,
+        });
+        break;
       case "01n":
-          this.setState({
-                  dir: one_night
-            });
-            break;
+        this.setState({
+          dir: one_night,
+        });
+        break;
       case "02d":
-          this.setState({
-                 dir: two_day
-               });
-            break;
+        this.setState({
+          dir: two_day,
+        });
+        break;
       case "02n":
-          this.setState({
-            dir: two_night,
-          });
-          break;
+        this.setState({
+          dir: two_night,
+        });
+        break;
       case "03d":
-          this.setState({
-            dir: three_day,
-          });
-          break;
+        this.setState({
+          dir: three_day,
+        });
+        break;
       case "03n":
-          this.setState({
-            dir: three_night,
-          });
-          break;
+        this.setState({
+          dir: three_night,
+        });
+        break;
       case "04d":
-          this.setState({
-            dir: four_day,
-          });
-          break;
+        this.setState({
+          dir: four_day,
+        });
+        break;
       case "04n":
-          this.setState({
-            dir: four_night,
-          });
-          break;
+        this.setState({
+          dir: four_night,
+        });
+        break;
       case "09d":
-          this.setState({
-            dir: nine_day,
-          });
-          break;
+        this.setState({
+          dir: nine_day,
+        });
+        break;
       case "09n":
-          this.setState({
-            dir: nine_night,
-          });
-          break;
+        this.setState({
+          dir: nine_night,
+        });
+        break;
       case "10n":
-          this.setState({
-            dir: ten_night,
-          });
-          break;
+        this.setState({
+          dir: ten_night,
+        });
+        break;
       case "10d":
-          this.setState({
-            dir: ten_day,
-          });
-          break;
+        this.setState({
+          dir: ten_day,
+        });
+        break;
       case "11d":
-          this.setState({
-            dir: eleven_day,
-          });
-          break;
+        this.setState({
+          dir: eleven_day,
+        });
+        break;
       case "11n":
-          this.setState({
-            dir: eleven_night,
-          });
-          break;
+        this.setState({
+          dir: eleven_night,
+        });
+        break;
       case "13d":
-          this.setState({
-            dir: thirteen_day,
-          });
-          break;
+        this.setState({
+          dir: thirteen_day,
+        });
+        break;
       case "13n":
-          this.setState({
-            dir: thirteen_night,
-          });
-          break;
+        this.setState({
+          dir: thirteen_night,
+        });
+        break;
       case "50d":
-          this.setState({
-            dir: fifty_day,
-          });
-          break;
+        this.setState({
+          dir: fifty_day,
+        });
+        break;
       case "50n":
-          this.setState({
-            dir: fifty_night,
-          });
-          break;
+        this.setState({
+          dir: fifty_night,
+        });
+        break;
       case "mist":
-          this.setState({
-            dir: mist,
-          });
-          break;
-      
-      default:
+        this.setState({
+          dir: mist,
+        });
         break;
 
-
-   }
-
-  }
+      default:
+        break;
+    }
+  };
 
   componentDidMount = () => {
+    // check whether an object is empty or not
+    function objEmptyCheck(value) {
+      return Object.keys(value).length === 0 && value.constructor === Object;
+    }
     try {
       let currentComponent = this;
-      if ("geolocation" in navigator) {
+      // Harsha's code - START
+      const context = this.context; // After component loads, get that context variable into "const context"
+
+      // For debugging purposes:
+      // -----------------------
+      // console.log("context: ", context);
+      // console.log(typeof context);
+      // console.log(context[0]);
+      // console.log(context[0].lat);
+      // console.log(objEmptyCheck(context[0]));
+
+      // First, check if that context variable is empty or not.
+      // If not empty, i.e., if it has some lat, lng values, then use axios to
+      // fetch OpenWeatherMap API using that lat, lng values.
+      // And then, set currentComponent state according to the response that we get.
+      if (!objEmptyCheck(context[0])) {
+        const WEATHER_URL = `https://api.openweathermap.org/data/2.5/weather?lat=${context[0].lat}&lon=${context[0].lng}&appid=${ApiKey}`;
+        axios.get(WEATHER_URL).then((response) => {
+          currentComponent.setState({
+            latitude: response.data.coord.lat,
+            longitude: response.data.coord.lon,
+            city: `${response.data.name}, ${response.data.sys.country}`,
+            temp_before: Math.floor(response.data.main.temp - 273.15),
+            feels: Math.floor(response.data.main.feels_like - 273.15),
+            date_time: moment.unix(response.data.dt).format("MMMM Do YYYY"),
+            description: response.data.weather[0].description,
+            icon: response.data.weather[0].icon,
+            day: moment.unix(response.data.dt).format("dddd"),
+          });
+        });
+      }
+
+      // Harsha's code - END
+      else if ("geolocation" in navigator) {
         navigator.geolocation.getCurrentPosition(async function (position) {
           var lat = position.coords.latitude;
           var lon = position.coords.longitude;
@@ -188,7 +222,7 @@ class Weather extends Component {
           ) {
             console.log("please enter valid city");
           }
-         
+
           currentComponent.setState({
             latitude: response.data.coord.lat,
             longitude: response.data.coord.lon,
@@ -199,17 +233,12 @@ class Weather extends Component {
             description: response.data.weather[0].description,
             icon: response.data.weather[0].icon,
             day: moment.unix(response.data.dt).format("dddd"),
-           
           });
-          
         });
       }
-     
     } catch (err) {
       console.log(err);
     }
-
-    
   };
   calCelcius(temp) {
     let celcius = Math.floor(temp - 273.15);
@@ -234,7 +263,7 @@ class Weather extends Component {
     ) {
       console.log("please enter valid city");
     }
-    
+
     console.log(response);
 
     this.setState({
@@ -247,9 +276,8 @@ class Weather extends Component {
       longitude: response.data.coord.lon,
       icon: response.data.weather[0].icon,
       day: moment.unix(response.data.dt).format("dddd"),
-      
     });
-  
+
     if (this.state.icon) {
       this.setBackground(this.state.icon);
     }
