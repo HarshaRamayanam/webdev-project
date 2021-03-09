3e0232ee462e3bd8d5786f8e969e11cc8d74fe93
sk192 <korulkar.shweta7@gmail.com>
2021-03-09 11:04:55 -0800

    imported css file

--- weather-app/src/components/weather_form/WeatherForm.js
+++ weather-app/src/components/weather_form/WeatherForm.js
@@ -1,39 +1,31 @@
 import React from "react";
-
+import '../weather_form/WeatherForm.css';
 
 const WeatherForm = (props) => {
   return (
     <div className="form_div">
-        <form id="addCityForm" onSubmit={props.loadweather} >
-          <div className="input-group form_row">
-              <input
-                type="text"
-                placeholder="City"
-                className="inputSearch form-control"
-                name="city"
-                id="addInput"
-                
-                required
-              />
-              
-              
+      <form id="addCityForm" onSubmit={props.loadweather}>
+        <div className="input-group form_row">
+          <input
+            type="text"
+            placeholder="City"
+            className="inputSearch form-control"
+            name="city"
+            id="addInput"
+            required
+          />
 
-            <div className="input-group-append">
-              <button type="submit" className="btnSearch btn btn-lg"> 
-                  <i class="fa fa-search" ></i>
-              </button>
-              
-            </div>
+          <div className="input-group-append">
+            <button type="submit" className="btnSearch btn btn-lg">
+              <i class="fa fa-search"></i>
+            </button>
           </div>
-          
-        </form>
-        <div className="lable_row ">
-            <label className=" searchLabel">Search by City Name</label>
         </div>
+      </form>
+      <div className="lable_row ">
+        <label className=" searchLabel">Search by City Name</label>
+      </div>
     </div>
-    
-
-    
   );
 };
 
